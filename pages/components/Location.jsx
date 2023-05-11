9ab64f24405d33baf80866d908ed2ebb5cbee22f
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2023-05-12 04:53:01 +0530

    Overview

--- pages/components/Location.jsx
+++ pages/components/Location.jsx
@@ -7,14 +7,14 @@ const Location = () => {
   return (
     <div
       id="Location"
-      className="relative flex flex-col ml-auto mr-auto top-20 p-10 md:flex-row-reverse xl:h-[749px]"
+      className="relative flex flex-col ml-auto mr-auto top-40 p-10 md:flex-row-reverse md:h-[749px]"
     >
-      <h1 className="xl:absolute xl:top-0 xl:right-52 text-center text-gray-700 text-xl xl:text-4xl xl:pt-8 font-bold">
+      <h1 className="md:absolute md:top-0 md:right-52 text-center text-gray-700 text-md md:text-4xl md:pt-8 font-bold">
         Location
       </h1>
-      <div className="relative md:px-4 max-w-3xl md:w-2/3 max-h-xl transition duration-1000 ease-in-out transform justify-center items-center content-center flex lg:right-0">
+      <div className="relative justify-center md:justify-left items-center content-center flex">
         <img
-          className="max-h-[500px] min-h-[300px] md:min-h-[400px] md:min-w-[400px] md:max-h-[700px] md:max-w-screen pl-6"
+          className="max-h-[500px] min-h-[300px] md:min-h-[400px] md:min-w-[400px]"
           src="location.png"
           alt=""
           // onClick={handleScrollToSection}
@@ -35,22 +35,22 @@ const Location = () => {
       </div> */}
       <div
         id="slider"
-        className="relative xl:justify-center overflow-x-scroll md:overflow-hidden scroll whitespace-nowrap scroll-smooth scrollbar-hide flex flex-row xl:flex-col mx-6 xl:w-1/2 py-10 xl:py-0 xl:h-full"
+        className="relative md:justify-center overflow-x-scroll md:overflow-hidden scroll whitespace-nowrap scroll-smooth scrollbar-hide flex flex-row md:flex-col mx-6 md:w-1/2 py-10 md:py-0 md:h-full"
       >
-        <div className="flex xl:py-3 flex-col xl:flex-row text-center xl:text-left items-center xl:w-auto xl:h-auto">
+        <div className="flex md:py-3 flex-col md:flex-row md:text-left items-center md:w-auto md:h-auto">
           <div
-            className="w-10 h-10 bg-contain bg-center xl:w-32 xl:h-20 xl:mr-6"
+            className="w-10 h-10 bg-contain  md:w-32 md:h-20 md:-6"
             style={{
               backgroundImage: "url(pic1.png)",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
           <div>
-            <div className="flex flex-col xl:flex-row">
-              <p className="text-xs xl:text-lg text-gray-700 xl:mt-2">
+            <div className="flex flex-col md:flex-row">
+              <p className="text-xs md:text-lg text-gray-700 md:mt-2">
                 Upcoming Whitefield
               </p>
-              <p className="text-xs pl-1 xl:text-lg text-gray-700 xl:mt-2">
+              <p className="text-xs pl-1 md:text-lg text-gray-700 md:mt-2">
                 Metro 6 min drive time
               </p>
             </div>
@@ -58,7 +58,7 @@ const Location = () => {
         </div>
         <div className="text-center">
           <div
-            className="w-[1px] xl:hidden mr-4 ml-4 h-24 xl:w-24 xl:h-24 xl:mr-6"
+            className="w-[1px] md:hidden mr-4 ml-4 h-24 md:w-24 md:h-24 md:mr-6"
             style={{
               backgroundImage: "url(line2.png)",
               backgroundRepeat: "no-repeat",
@@ -66,20 +66,20 @@ const Location = () => {
             }}
           ></div>
         </div>
-        <div className="flex xl:py-3 flex-col xl:flex-row text-center xl:text-left items-center xl:w-auto xl:h-auto">
+        <div className="flex md:py-3 flex-col md:flex-row text-center md:text-left items-center md:w-auto md:h-auto">
           <div
-            className="w-10 h-10 bg-contain bg-center xl:w-32 xl:h-20 xl:mr-6"
+            className="w-10 h-10 bg-contain  md:w-32 md:h-20 md:mr-6"
             style={{
               backgroundImage: "url(pic2.png)",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
           <div>
-            <div className="flex flex-col xl:flex-row">
-              <p className="text-xs xl:text-lg text-gray-700 xl:mt-2">
+            <div className="flex flex-col md:flex-row">
+              <p className="text-xs md:text-lg text-gray-700 md:mt-2">
                 Manipal Hospital 20
               </p>
-              <p className="text-xs pl-1 xl:text-lg text-gray-700 xl:mt-2">
+              <p className="text-xs pl-1 md:text-lg text-gray-700 md:mt-2">
                 min drive time
               </p>
             </div>
@@ -87,7 +87,7 @@ const Location = () => {
         </div>
         <div className="text-center">
           <div
-            className="w-[1px] xl:hidden mr-4 ml-4 h-24 xl:w-24 xl:h-24 xl:mr-6"
+            className="w-[1px] md:hidden mr-4 ml-4 h-24 md:w-24 md:h-24 md:mr-6"
             style={{
               backgroundImage: "url(line2.png)",
               backgroundRepeat: "no-repeat",
@@ -95,20 +95,20 @@ const Location = () => {
             }}
           ></div>
         </div>
-        <div className="flex xl:py-3 flex-col xl:flex-row text-center xl:text-left items-center xl:w-auto xl:h-auto">
+        <div className="flex md:py-3 flex-col md:flex-row text-center md:text-left items-center md:w-auto md:h-auto">
           <div
-            className="w-10 h-10 bg-contain bg-center xl:w-32 xl:h-20 xl:mr-6"
+            className="w-10 h-10 bg-contain  md:w-32 md:h-20 md:mr-6"
             style={{
               backgroundImage: "url(pic3.png)",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
           <div>
-            <div className="flex flex-col xl:flex-row">
-              <p className="text-xs xl:text-lg text-gray-700 xl:mt-2">
+            <div className="flex flex-col md:flex-row">
+              <p className="text-xs md:text-lg text-gray-700 md:mt-2">
                 Taj Vivanta 15 min
               </p>
-              <p className="text-xs pl-1 xl:text-lg text-gray-700 xl:mt-2">
+              <p className="text-xs pl-1 md:text-lg text-gray-700 md:mt-2">
                 drive time
               </p>
             </div>
@@ -116,7 +116,7 @@ const Location = () => {
         </div>
         <div className="text-center">
           <div
-            className="w-[1px] xl:hidden mr-4 ml-4 h-24 xl:w-24 xl:h-24 xl:mr-6"
+            className="w-[1px] md:hidden mr-4 ml-4 h-24 md:w-24 md:h-24 md:mr-6"
             style={{
               backgroundImage: "url(line2.png)",
               backgroundRepeat: "no-repeat",
@@ -124,20 +124,20 @@ const Location = () => {
             }}
           ></div>
         </div>
-        <div className="flex xl:py-3 flex-col xl:flex-row text-center xl:text-left items-center xl:w-auto xl:h-auto">
+        <div className="flex md:py-3 flex-col md:flex-row text-center md:text-left items-center md:w-auto md:h-auto">
           <div
-            className="w-10 h-10 bg-contain bg-center xl:w-32 xl:h-20 xl:mr-6"
+            className="w-10 h-10 bg-contain  md:w-32 md:h-20 md:mr-6"
             style={{
               backgroundImage: "url(pic4.png)",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
           <div>
-            <div className="flex flex-col xl:flex-row">
-              <p className="text-xs xl:text-lg text-gray-700 xl:mt-2">
+            <div className="flex flex-col md:flex-row">
+              <p className="text-xs md:text-lg text-gray-700 md:mt-2">
                 Orchids International
               </p>
-              <p className="text-xs xl:text-lg pl-1 text-gray-700 xl:mt-2">
+              <p className="text-xs md:text-lg pl-1 text-gray-700 md:mt-2">
                 School 3 min drive time
               </p>
             </div>
@@ -145,7 +145,7 @@ const Location = () => {
         </div>
         <div className="text-center">
           <div
-            className="w-[1px] xl:hidden mr-4 ml-4 h-24 xl:w-24 xl:h-24 xl:mr-6"
+            className="w-[1px] md:hidden mr-4 ml-4 h-24 md:w-24 md:h-24 md:mr-6"
             style={{
               backgroundImage: "url(line2.png)",
               backgroundRepeat: "no-repeat",
@@ -153,20 +153,20 @@ const Location = () => {
             }}
           ></div>
         </div>
-        <div className="flex xl:py-3 flex-col xl:flex-row text-center xl:text-left items-center xl:w-auto xl:h-auto">
+        <div className="flex md:py-3 flex-col md:flex-row text-center md:text-left items-center md:w-auto md:h-auto">
           <div
-            className="w-10 h-10 bg-contain bg-center xl:w-32 xl:h-20 xl:mr-6"
+            className="w-10 h-10 bg-contain  md:w-32 md:h-20 md:-6"
             style={{
               backgroundImage: "url(pic5.png)",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
           <div>
-            <div className="flex flex-col xl:flex-row">
-              <p className="text-xs xl:text-lg text-gray-700 xl:mt-2">
+            <div className="flex flex-col md:flex-row">
+              <p className="text-xs md:text-lg text-gray-700 md:mt-2">
                 ITPB 16 min drive
               </p>
-              <p className="text-xs pl-1 xl:text-lg text-gray-700 xl:mt-2">
+              <p className="text-xs pl-1 md:text-lg text-gray-700 md:mt-2">
                 time
               </p>
             </div>
